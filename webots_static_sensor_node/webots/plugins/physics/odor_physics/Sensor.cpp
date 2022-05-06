1b4faa48b369363e91343f9341a5d23220753577
inalbon <malika.in-albon@epfl.ch>
2022-05-06 09:29:48 +0200

    Added wind map so simulation is working

--- webots_static_sensor_node/webots/plugins/physics/odor_physics/Sensor.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2005-2008, Thomas Lochmatter, thomas.lochmatter@epfl.ch
-// Documentation: http://en.wikibooks.org/wiki/Webots_Odor_Simulation
-
-#include "Sensor.h"
-#define THISCLASS Sensor
